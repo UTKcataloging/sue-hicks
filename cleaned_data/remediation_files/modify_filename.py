b29584df495824ea0d7e7e7630b466c72e6d345b
Mark Baggett <mbagget1@utk.edu>
2017-06-15 14:50:24 -0400

    Show query first!

--- cleaned_data/remediation_files/modify_filename.py
+++ cleaned_data/remediation_files/modify_filename.py
@@ -13,12 +13,13 @@ credentials = {"username": "user", "password": "pass"}
 def update_filenames(x, url):
     for item in os.walk(x):
         for record in item[2]:
+            print("Looking for {0}".format(record))
             r = requests.get("{0}?query=identifier%7E*{1}*&pid=true&resultFormat=xml".format(url, record.strip('.xml')),
                              auth=(credentials['username'], credentials['password']))
             json_string = json.dumps(xmltodict.parse(r.text))
             json_document = json.loads(json_string)
             pid = json_document["result"]["resultList"]["objectFields"]["pid"]
-            print("Moving {0} to {1}".format(record,json_document["result"]["resultList"]["objectFields"]["pid"]))
+            print("\tMoving {0} to {1}.".format(record,json_document["result"]["resultList"]["objectFields"]["pid"]))
             shutil.copyfile("{0}{1}".format(x,record), "output/{0}.xml".format(pid))
 
 if __name__ == "__main__":
